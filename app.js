c52191944f73c11bf157eaed3d771a1b5952c7d3
Nerea Valdés Egocheaga <nerevalego@gmail.com>
2020-03-21 23:07:04 +0100

    SDI-NodeJS-8.2 - Acceso a datos de Mongo desde Node.js

--- app.js
+++ app.js
@@ -2,19 +2,21 @@
 let express = require('express');
 let app = express();
 
+let mongo = require('mongodb');
 let swig = require('swig');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-
 app.use(express.static('public'));
 
 // Variables
 app.set('port', 8081);
+app.set('db', 'mongodb://admin:Sumpeti4@tiendamusica-shard-00-00-nukgt.mongodb.net:27017,tiendamusica-shard-00-01-nukgt.mongodb.net:27017,tiendamusica-shard-00-02-nukgt.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true&w=majority');
+
 
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app, swig);
+require("./routes/rcanciones.js")(app, swig, mongo);
 require("./routes/rautores.js")(app, swig);
 
 // lanzar el servidor
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,4 +1,4 @@
-module.exports = function (app, swig) {
+module.exports = function (app, swig, mongo) {
 
     app.get("/canciones", function (req, res) {
         var canciones = [{
@@ -30,11 +30,6 @@ module.exports = function (app, swig) {
         res.send(String(respuesta));
     });
 
-    app.get('/canciones/:id', function (req, res) {
-        let respuesta = 'id: ' + req.params.id;
-        res.send(respuesta);
-    });
-
     app.get('/canciones/:genero/:id', function (req, res) {
         let respuesta = 'id: ' + req.params.id + '<br>'
             + 'Género: ' + req.params.genero;
@@ -42,8 +37,33 @@ module.exports = function (app, swig) {
     });
 
     app.post("/cancion", function (req, res) {
-        res.send("Canción agregada:" + req.body.nombre + "<br>"
-            + " genero : " + req.body.genero + "<br>"
-            + " precio: " + req.body.precio);
-    })
+        let cancion = {
+            nombre : req.body.nombre,
+            genero : req.body.genero,
+            precio : req.body.precio
+        }
+        // Conectarse
+        mongo.MongoClient.connect(
+            app.get('db'),
+            function(err, db) {
+                if (err) {
+                    res.send("Error de conexión: " + err);
+                } else {
+                    let collection = db.collection('canciones');
+                    collection.insert(cancion, function(err, result) {
+                        if (err) {
+                            res.send("Error al insertar " + err);
+                        } else {
+                            res.send("Agregada id:  "+ result.ops[0]._id);
+                        }
+                        db.close();
+                    });
+                }
+            });
+        })
+
+    app.get('/canciones/:id', function (req, res) {
+        let respuesta = 'id: ' + req.params.id;
+        res.send(respuesta);
+    });
 };
