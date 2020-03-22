96b134e28ca7336e59ea95c856705523ab741a5e
Nerea Valdés Egocheaga <nerevalego@gmail.com>
2020-03-22 01:18:41 +0100

    SDI-NodeJS-8.9 y 8.10 - Colecciones relacionadas y routers

--- app.js
+++ app.js
@@ -15,11 +15,47 @@ let mongo = require('mongodb');
 let swig = require('swig');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 let gestorDB = require("./modules/gestorDB.js");
 gestorDB.init(app, mongo);
+
+// routerUsuarioSession
+let routerUsuarioSession = express.Router();
+routerUsuarioSession.use(function (req, res, next) {
+    console.log("routerUsuarioSession");
+    if (req.session.usuario) {
+        // dejamos correr la petición
+        next();
+    } else {
+        console.log("va a : "+req.session.destino);
+        res.redirect("/identificarse");
+    }
+});
+
+//Aplicar routerUsuarioSession
+app.use("/canciones/agregar",routerUsuarioSession);
+app.use("/publicaciones",routerUsuarioSession);
+
+//routerAudios
+let routerAudios = express.Router();
+routerAudios.use(function(req, res, next) {
+    console.log("routerAudios");
+    let path = require('path');
+    let idCancion = path.basename(req.originalUrl, '.mp3');
+    gestorBD.obtenerCanciones(
+        {"_id": mongo.ObjectID(idCancion) },
+        function (canciones) {
+            if(req.session.usuario && canciones[0].autor == req.session.usuario ){
+                next();
+            } else {
+                res.redirect("/tienda");
+            }
+        })
+});
+
+//Aplicar routerAudios
+app.use("/audios/",routerAudios);
 
 // Variables
 app.set('port', 8081);
@@ -32,7 +68,9 @@ require("./routes/rusuarios.js")(app, swig, gestorDB); // (app, param1, param2, 
 require("./routes/rcanciones.js")(app, swig, gestorDB);
 require("./routes/rautores.js")(app, swig);
 
+app.use(express.static('public'));
+
 // lanzar el servidor
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log("Servidor activo");
 });
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -20,16 +20,12 @@ module.exports = function (app, swig, gestorDB) {
     })
 
     app.get('/canciones/agregar', function (req, res) {
-        if( req.session.usuario == null){
-            res.redirect("/tienda");
-            return;
-        }
         let respuesta = swig.renderFile('views/bagregar.html', {});
         res.send(respuesta);
     })
 
     app.get('/cancion/:id', function (req, res) {
-        let criterio = { "_id" : gestorDB.mongo.ObjectID(req.params.id) };
+        let criterio = {"_id": gestorDB.mongo.ObjectID(req.params.id)};
 
         gestorDB.obtenerCanciones(criterio, function (canciones) {
             if (canciones == null) {
@@ -44,6 +40,77 @@ module.exports = function (app, swig, gestorDB) {
         });
     })
 
+    app.get('/cancion/modificar/:id', function (req, res) {
+        let criterio = {"_id": gestorDB.mongo.ObjectID(req.params.id)};
+        gestorDB.obtenerCanciones(criterio, function (canciones) {
+            if (canciones == null) {
+                res.send(respuesta);
+            } else {
+                let respuesta = swig.renderFile('views/bcancionModificar.html', {
+                    cancion: canciones[0]
+                });
+                res.send(respuesta);
+            }
+        });
+    })
+
+    app.post('/cancion/modificar/:id', function (req, res) {
+        let id = req.params.id;
+        let criterio = {"_id": gestorDB.mongo.ObjectID(id)};
+        let cancion = {
+            nombre: req.body.nombre, genero: req.body.genero,
+            precio: req.body.precio
+        }
+
+        gestorDB.modificarCancion(criterio, cancion, function (result) {
+            if (result == null) {
+                res.send("Error al modificar ");
+            } else {
+                paso1ModificarPortada(req.files, id, function (result) {
+                    if (result == null) {
+                        res.send("Error en la modificación");
+                    } else {
+                        res.send("Modificado");
+                    }
+                });
+            }
+        });
+    })
+
+    function paso1ModificarPortada(files, id, callback) {
+        if (files && files.portada != null) {
+            let imagen = files.portada;
+            imagen.mv('public/portadas/' + id + '.png', function (err) {
+                if (err) {
+                    callback(null); // ERROR
+                } else {
+                    paso2ModificarAudio(files, id, callback);
+                    // SIGUIENTE
+                }
+            });
+        } else {
+            paso2ModificarAudio(files, id, callback);
+            // SIGUIENTE
+        }
+    };
+
+    function paso2ModificarAudio(files, id, callback) {
+        if (files && files.audio != null) {
+            let audio = files.audio;
+            audio.mv('public/audios/' + id + '.mp3', function (err) {
+                if (err) {
+                    callback(null);
+                    // ERROR
+                } else {
+                    callback(true);
+                    // FIN
+                }
+            });
+        } else {
+            callback(true); // FIN
+        }
+    };
+
     app.get('/suma', function (req, res) {
         let respuesta = parseInt(req.query.num1)
             + parseInt(req.query.num2);
@@ -57,10 +124,6 @@ module.exports = function (app, swig, gestorDB) {
     })
 
     app.post("/cancion", function (req, res) {
-        if( req.session.usuario == null){
-            res.redirect("/tienda");
-            return;
-        }
         let cancion = {
             nombre: req.body.nombre,
             genero: req.body.genero,
@@ -112,4 +175,18 @@ module.exports = function (app, swig, gestorDB) {
             }
         });
     });
-};
+
+    app.get("/publicaciones", function (req, res) {
+        let criterio = {autor: req.session.usuario};
+
+        gestorDB.obtenerCanciones(criterio, function (canciones) {
+            if (canciones == null) {
+                res.send("Error al listar ");
+            } else {
+                let respuesta = swig.renderFile('views/bpublicaciones.html', {canciones: canciones});
+                res.send(respuesta);
+            }
+        });
+    });
+}
+;
