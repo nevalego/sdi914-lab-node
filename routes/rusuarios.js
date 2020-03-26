fde9fa61797f7ef4099bd1f028d9fd46c66c6a13
Nerea Valdés Egocheaga <nerevalego@gmail.com>
2020-03-26 12:48:30 +0100

    SDI-NodeJS-9.4- Mensajes y alertas

--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -19,9 +19,9 @@ module.exports = function (app, swig, gestorDB) {
         }
         gestorDB.insertarUsuario(usuario, function (id) {
             if (id == null) {
-                res.send("Error al insertar el usuario");
+                res.redirect("/registrarse?mensaje=Error al registrar usuario");
             } else {
-                res.redirect("/identificarse");
+                res.redirect("/identificarse?mensaje=Nuevo usuario registrado");
             }
         })
     });
@@ -40,7 +40,9 @@ module.exports = function (app, swig, gestorDB) {
         gestorDB.obtenerUsuarios(criterio, function (usuarios) {
             if (usuarios == null || usuarios.length == 0) {
                 req.session.usuario = null;
-                res.send("No identificado: ");
+                res.redirect("/identificarse" +
+                    "?mensaje=Email o password incorrecto"+
+                    "&tipoMensaje=alert-danger ");
             } else {
                 req.session.usuario = usuarios[0].email;
                 res.redirect("/publicaciones");
