2424f0f427e9d17580edb5c81041368845e6c7c2
Nerea Valdés Egocheaga <nerevalego@gmail.com>
2020-03-26 18:55:33 +0100

    SDI-NodeJS-9.7 y 9.8- Manejo de errores en la aplicación e implementando HTTPS

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 let express = require('express');
 let app = express();
 
+let fs = require('fs');
+let https = require('https');
+
 let expressSession = require('express-session');
 app.use(expressSession({
     secret: 'abcdefg',
@@ -112,7 +115,18 @@ app.get('/', function (req,res) {
     res.redirect('/tienda');
 })
 
+app.use(function(err, req, res, next){
+   console.log("Error producido: "+err);// mostramos el erroe en consola
+   if( ! res.headersSent){
+       res.status(400);
+       res.send("Recurso no disponible");
+   }
+});
+
 // lanzar el servidor
-app.listen(app.get('port'), function () {
+https.createServer({
+    key: fs.readFileSync('certificates/alice.key'),
+    cert: fs.readFileSync('certificates/alice.crt')
+}, app).listen(app.get('port'), function() {
     console.log("Servidor activo");
 });
